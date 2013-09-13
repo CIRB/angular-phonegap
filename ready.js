111a66f1bc921ad9b8373963c38ba9bf3a0eafd5
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2013-09-13 12:41:46 +0200

    deviceready using promise

--- ready.js
+++ ready.js
@@ -1,37 +1,19 @@
-/*
- * angular-phonegap-ready v0.0.1
- * (c) 2013 Brian Ford http://briantford.com
- * License: MIT
- */
-
-
-angular.module('btford.phonegap.ready', []).
-  factory('phonegapReady', function ($rootScope) {
+(function () {
     'use strict';
-    var deviceready = false;
-    angular.element(document).bind('deviceready', function () {
-      deviceready = true;
+    var defered = null;
+
+    angular.module('irisnet.phonegap', []).
+        factory('phonegapReady', function ($rootScope, $q) {
+
+        if (!defered) {
+            defered = $q.defer();
+            angular.element(document).bind('deviceready', function () {
+                $rootScope.$apply(defered.resolve());
+            });
+        }
+
+        return function () {
+            return defered.promise;
+        };
     });
-    return function (fn) {
-      var queue = [];
-  
-      var impl = function () {
-        queue.push(Array.prototype.slice.call(arguments));
-      };
-  
-      if (!deviceready) {
-        angular.element(document).bind('deviceready', function () {
-          queue.forEach(function (args) {
-            fn.apply(this, args);
-          });
-          impl = fn;
-        });
-      } else {
-        impl = fn;
-      }
-  
-      return function () {
-        return impl.apply(this, arguments);
-      };
-    };
-  });
+})();
