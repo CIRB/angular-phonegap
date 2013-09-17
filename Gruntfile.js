1b412229c35729105b66b559e5f4b22c830c7331
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2013-09-17 09:29:55 +0200

    grunt...

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
     release: {
       options: {
         npm: false,
-        file: ['bower.json', 'package.json']
+        file: ['bower.json']
       }
     }
   });
