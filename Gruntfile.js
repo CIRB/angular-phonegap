a51e09f867f1d097e0ccb6f1fb2ed2c9584165cf
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2013-09-17 09:19:12 +0200

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    jshint: {
+      options: {
+        globals: {
+          "angular": true
+        }
+      },
+      lint: {
+        src: ['Gruntfile.js', '**/*.js']
+      }
+    },
+    watch: {
+      console: {
+        files: [
+            '<%= jshint.lint.src %>'
+        ],
+        tasks: ['default']
+      },
+      release: {
+        options: {
+          npm: false,
+          file: ['bower.json', 'package.json']
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-release');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+};
